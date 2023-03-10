5810d211ce9b8b23d5e4b04839f0a0d390b0f970
cloud866 <dklein927@gmail.com>
2023-03-10 19:51:39 +0300

    Initial commit

---
+++ src/Bank.java
@@ -0,0 +1,49 @@
+public class Bank {
+
+    private int money = 10000;
+    private Object lock = new Object();
+
+    int getMoney() {
+        return money;
+    }
+
+    void take(int money) {
+        synchronized (lock) {
+            this.money -= money;
+        }
+
+    }
+
+    void repay(int money) {
+        synchronized (lock) {
+            this.money += money;
+        }
+    }
+
+    class Client extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                if (getMoney() >= 1000) {
+                    take(1000);
+                    repay(1000);
+                }
+            }
+        }
+    }
+
+    public Bank() {
+        new Client().start();
+        new Client().start();
+        new Client().start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Bank bank = new Bank();
+        while(true) {
+            System.out.println(bank.money);
+            Thread.sleep(1000);
+        }
+    }
+
+}
